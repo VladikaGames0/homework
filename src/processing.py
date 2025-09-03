108dc3ed21414d5df4076d3bf84a9ca9dcb02600
vroma <vromasov5@gmail.com>
2025-09-03 13:30:59 +0700

    create moduls sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,6 @@
 def filter_by_state(data_list, state='EXECUTED'):
     return [item for item in data_list if item.get('state') == state]
+
+
+def sort_by_date(sort_data_list, descending=True):
+    return sorted(sort_data_list, key=lambda x: x.get('date'), reverse=descending)
