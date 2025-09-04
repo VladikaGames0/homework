79c1ba459ce7205ed7c7fac5ffde19e71beaaee0
vroma <vromasov5@gmail.com>
2025-09-04 12:32:57 +0700

    update type_annotation

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,23 +1,28 @@
 import pytest
-from src.masks import get_mask_account,get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
-@pytest.mark.parametrize("input_val", [
-    "12345",
-    "",
-    "1234567890",
-])
-
-def test_edge_cases(input_val):
+@pytest.mark.parametrize(
+    "input_val",
+    [
+        "12345",
+        "",
+        "1234567890",
+    ],
+)
+def test_edge_cases(input_val: str) -> None:
     """Проверяем что функция не падает и возвращает корректный формат"""
     result = get_mask_card_number(input_val)
-    # Длина результата должна быть (len(input) при длине ≤10, но функция не рассчитана на это)
     assert isinstance(result, str)
     assert all(c.isdigit() or c == "*" or c == " " for c in result)
 
 
-@pytest.mark.parametrize("input_val, expected", [("", "** "),
-    ])
-def test_get_mask_account(input_val, expected):
-    """Если пустая строка, то  — возвращает '**'"""
+@pytest.mark.parametrize(
+    "input_val, expected",
+    [
+        ("", " "),
+    ],
+)
+def test_get_mask_account(input_val: str, expected: str) -> None:
+    """Если пустая строка, то  — возвращает ''"""
     assert get_mask_account(input_val) == expected
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,12 +1,10 @@
-import pytest
-from src.processing import filter_by_state,sort_by_date
+from src.processing import filter_by_state, sort_by_date
 
-def test_filter_empty_list():
+def test_filter_empty_list()-> None:
     """Проверяет пустой список"""
     assert filter_by_state([]) == []
 
 
-def test_sort_empty_list():
+def test_sort_empty_list()-> None:
     """Проверяет пустой список"""
     assert sort_by_date([], descending=True) == []
-
