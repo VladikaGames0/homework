1e89413366f3ade9c8ba54fbb8a957848ce90302
vroma <vromasov5@gmail.com>
2025-09-04 11:59:03 +0700

    create func test_widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,7 +2,12 @@ import pytest
 from src.widget import mask_account_card,get_date
 
 @pytest.mark.parametrize("input_val, expected", [
-    ("Счет 1234567890", "Счет **7890"),
+    ("Счет 123", "Счет **123"),
 ])
 def test_mask_account_card(input_val, expected):
+    """Проверяем, если счёт меньше 4 символов"""
     assert mask_account_card(input_val) == expected
+
+def test_get_date_no_time():
+    """Проверяем, если нет времени"""
+    assert get_date("2024-06-24") == "24.06.2024"
