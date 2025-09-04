825cefad3f74eb33275acfbd3ec802e10fc658f4
vroma <vromasov5@gmail.com>
2025-09-04 12:16:56 +0700

    update test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -8,6 +8,16 @@ def test_mask_account_card(input_val, expected):
     """Проверяем, если счёт меньше 4 символов"""
     assert mask_account_card(input_val) == expected
 
+
 def test_get_date_no_time():
     """Проверяем, если нет времени"""
     assert get_date("2024-06-24") == "24.06.2024"
+
+
+def test_mask_account_card_without_schet():
+    """Проверяет, если не написанно 'Счет'"""
+    assert mask_account_card("Visa 1234567812345678") == "Visa 1234567812345678"
+    assert mask_account_card(1234567890) == "1234567890"
+
+
+
