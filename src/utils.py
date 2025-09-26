1eb9c3af32e8f4fac8248dbc3163ed3221ee318f
vroma <vromasov5@gmail.com>
2025-09-26 17:21:33 +0700

    crate update external_api

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
 import json
+import logging
 from typing import List, Dict
+
+logger = logging.getLogger("utils")
 
 def load_transactions(file_path: str) -> List[Dict]:
     """Загружает данные из JSON-файла с финансовыми транзакциями."""
@@ -7,8 +10,14 @@ def load_transactions(file_path: str) -> List[Dict]:
         with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
         if isinstance(data, list):
+            logger.info(f"Успешно загружены транзакции из файла: {file_path}")
             return data
         else:
+            logger.warning(f"Данные в файле {file_path} имеют неверный формат (не список).")
             return []
-    except (FileNotFoundError, json.JSONDecodeError):
+    except FileNotFoundError:
+        logger.error(f"Файл не найден: {file_path}")
+        return []
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON из файла: {file_path}")
         return []
