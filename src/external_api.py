1dcb1f7f140a0d72a1a46b489bbee54830de87f0
vroma <vromasov5@gmail.com>
2025-10-03 14:44:51 +0700

    crate external_api and test_external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("EXCHANGE_RATES_API_KEY")
+
+def convert_to_rub(transaction):
+    """Конвертирует сумму транзакции в рубли, если валюта отличается от RUB."""
+    amount = transaction['operationAmount']['amount']
+    currency = transaction['operationAmount']['currency']['code']
+
+    if currency == 'RUB':
+        return float(amount)
+    elif currency in ('USD', 'EUR'):
+        exchange_rate = get_exchange_rate(currency)
+        if exchange_rate:
+            return float(amount) * exchange_rate
+        else:
+            return None  # Или обработать ошибку, например, вернуть исходную сумму
+
+def get_exchange_rate(currency):
+    """Получает текущий курс валюты к рублю через API."""
+    url = f'https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}'
+    headers = {"apikey": API_KEY}  # Используем API key из .env
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Проверка на ошибки HTTP
+        data = response.json()
+        return data['rates']['RUB']
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе к API: {e}")
+        return None
